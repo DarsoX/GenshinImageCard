4c0705e8a2d646dfc4e2376fe7489c5e66b20cb5
DarsoX <43499767+DarsoX@users.noreply.github.com>
2022-04-27 23:38:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,54 @@
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
-import vk_api
-import requests
-
+import collections
 import asyncio
 import genshin
 
-vk = vk_api.VkApi(token = "fb1a8225db7222b4e52ea4dbab8fc4b5b05163122069832ea2c8e6b8640c59b818ae0e6a8e51673c6cefa")
-
-vkpril = vk_api.VkApi(token = "5e380fc25e380fc25e380fc2675e444fd555e385e380fc23c67269f3cb20e68698358ef")
-
-
 slot_size = (267,267)
 
-bg = Image.open('card/cart.png')
-z_bg = Image.open('card/cart.png')
-font_stats = ImageFont.truetype("card/Genshin_Impact.ttf", 57)
-font_name = ImageFont.truetype("card/Genshin_Impact.ttf", 34)
-font_name_text = ImageFont.truetype("card/Genshin_Impact.ttf", 24)
+bg = Image.open('cart.png')
+z_bg = Image.open('cart.png')
+font_stats = ImageFont.truetype("Genshin_Impact.ttf", 57)
+font_name = ImageFont.truetype("Genshin_Impact.ttf", 34)
+font_name_text = ImageFont.truetype("Genshin_Impact.ttf", 24)
 
-ava = Image.open('card/ava.png')
-ava.thumbnail(slot_size)
+avatar = Image.open('card/ava.png')
+avatar.thumbnail(slot_size)
 
 async def up_img(img):
     bytes = BytesIO()
 
-    img.save(bytes, format='png')
-
-    url = vk.method('photos.getMessagesUploadServer')['upload_url']
-    response = vk.http.post(url, files = [('file', ('file.png', bytes.getvalue()))])
-    response = vk.method('photos.saveMessagesPhoto', response.json())
-
-    return 'photo{}_{}'.format(response[0]['owner_id'], response[0]['id'])
-
+    img.save("Card.png")
 
 async def get_client(Hid,HtokenId):
     try:
         cookies = {"ltuid": Hid, "ltoken": HtokenId}
         client = genshin.Client(cookies)
         data = await client.get_record_card(Hid)
-        return data.uid, data.level, data.nickname,data.server_name
+        
+        info: dict[str, int] = collections.namedtuple('GenStats', ['uid','level','nickname','server_name'])
+        GenStats = info(data.uid, data.level, data.nickname,data.server_name)
+        
+        return GenStats
+    
     except:
-        return None,None,None,None
+        return None
+    
 class PlayerСard(object):
     def __init__(self, HtokenId:str = '', Hid:int = 0):
         super(PlayerСard, self).__init__()
         if HtokenId != '' and Hid != 0:
             self.HtokenId = HtokenId
             self.Hid = Hid
-            self.uid, self.level, self.nickname, self.server_name = asyncio.run(get_client(Hid,HtokenId))
+            GenStats = asyncio.run(get_client(Hid,HtokenId))
+            self.uid = GenStats.uid, 
+            self.level = GenStats.level
+            self.nickname = GenStats.nickname
+            self.server_name = GenStats.server_name
             self.cookies = {"ltuid": Hid, "ltoken": HtokenId}
         else:
             return False
-    async def get_name_vk(self,from_id:int = 0):
-        info = vkpril.method('users.get', {'user_ids': from_id})[0]
-        
-        return f"{info['first_name']} {info['last_name']}"
-
-    async def get_photo(self,from_id:int = 0):
-        if from_id != 0:
-            url = vkpril.method('users.get', {'user_ids': from_id, 'fields': 'photo_max_orig'})[0]['photo_max_orig']
-            if url != 'https://vk.com/images/camera_200.png':
-                img = Image.open(BytesIO(requests.get(url).content))
-                img.thumbnail(slot_size)
-                return img
-        return ava
     
     async def get_genshinStats(self, fuid:int = 0):
         uid = self.uid
@@ -102,16 +85,8 @@ class PlayerСard(object):
         return achievements, days_active, characters,spiral_abyss,anemoculi,geoculi,electroculi,common_chests,exquisite_chests,precious_chests,luxurious_chests,remarkable_chests,unlocked_waypoints,unlocked_domains, uid,level,nickname, server_name
 
 
-    async def creat(self, from_id: int = 0, frend_hid: int = 0):
+    async def creat(self, frend_hid: int = 0):
         achievements, days_active, characters,spiral_abyss,anemoculi,geoculi,electroculi,common_chests,exquisite_chests,precious_chests,luxurious_chests,remarkable_chests,unlocked_waypoints,unlocked_domains, uid,level,nickname,server_name = await self.get_genshinStats(fuid = frend_hid)
-        avatar = await self.get_photo(from_id)
-        if from_id != 0:
-            name_vk = await self.get_name_vk(from_id)
-            vk_id = f"vk.com/id{from_id}"
-        else:
-            name_vk = "Genshin Imapct Bot"
-            vk_id = "vk.com/bot.genshin"
-
         # Avatar Add
         img = bg.copy()
         img.paste(avatar, (59,153))
@@ -122,9 +97,6 @@ class PlayerСard(object):
         text.text((354,226), f"{nickname} | {level} Lvl", font=font_name, fill=(255,255,255,255))
 
         text.text((354,273), f"{server_name.replace('Server', '')} | UID: {uid}", font=font_name_text, fill=(255,255,255,255))
-        text.text((354,307), name_vk, font=font_name_text, fill=(255,255,255,255))
-        text.text((354,336), vk_id, font=font_name_text, fill=(255,255,255,255))
-
 
         # Name Stats Add
         #1
